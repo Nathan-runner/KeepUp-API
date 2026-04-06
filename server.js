0cc09d62f562d56a4c776a7c870d88af9ee2bc84
Nathan-runner <rozorinho@gmail.com>
2026-04-06 01:04:55 -0300

    add the fallback

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const app = require('./app.js');
 const sequelize = require('./src/config/database.js');
 
+const PORT = process.env.PORT || 3000;
+
 sequelize.sync().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("Servidor rodando 🚀");
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT} 🚀`);
   });
 });
